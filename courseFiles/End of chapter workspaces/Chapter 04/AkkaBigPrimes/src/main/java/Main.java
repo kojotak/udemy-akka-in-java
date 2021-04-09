8483590a43239052851dc07fe3a2fdfdbd5334c7
kojotak <tomas.beneda@gmail.com>
2021-04-09 12:22:07 +0200

    cleanup

--- courseFiles/End of chapter workspaces/Chapter 04/AkkaBigPrimes/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import akka.actor.typed.ActorSystem;
-
-public class Main {
-    public static void main(String[] args) {
-       ActorSystem<String> actorSystem = ActorSystem.create(FirstSimpleBehavior.create(),"FirstActorSystem");
-       actorSystem.tell("Hello are you there?");
-       actorSystem.tell("This is the second message.");
-    }
-}
