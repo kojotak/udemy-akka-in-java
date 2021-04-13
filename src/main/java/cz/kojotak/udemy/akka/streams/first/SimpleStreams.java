1c1237b3ec26cc19ac96c7f6260308f28161b9f1
kojotak <tomas.beneda@gmail.com>
2021-04-13 18:46:36 +0200

    akka streams chapter 3

--- src/main/java/cz/kojotak/udemy/akka/streams/first/SimpleStreams.java
+++ src/main/java/cz/kojotak/udemy/akka/streams/first/SimpleStreams.java
@@ -1,6 +1,9 @@
 package cz.kojotak.udemy.akka.streams.first;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.CompletionStage;
+import java.util.stream.Stream;
 
 import akka.Done;
 import akka.NotUsed;
@@ -18,19 +21,38 @@ public class SimpleStreams {
 		//integer - co produkuju, NotUsed - materialized value
 		Source<Integer, NotUsed> source = Source.range(1, 10);
 		
+//		finite sources
+//		Source.single(5);
+//		Source.range(1, 10, 2); //ob jedno
+//		Source.from( List.of(1,3,5,7) ); // z listu
+		
+//		infinite sources
+//		Source.repeat(42);
+//		Source.cycle( ()->List.of(1,2,3).iterator() ); //nekonecne se opakujici trojice
+//		Source.fromIterator( ()-> Stream.iterate(0, i->i+1).iterator() ); //z java streamu
+		
 		Flow<Integer, String, NotUsed> flow = Flow.of(Integer.class) //of(Integer) - pro vynucenou typovou kontrolu
 				.map( v -> "The next value is "+v );  //samotny obsah flow
+		
+//		operator
+//		source.throttle(1, Duration.ofSeconds(3)); //davkuje se zpozdenim
+//		source.take(2); //staci nam 2 a pak se zastavi - omezovac
 		
 		//string - co konzumuju
 		Sink<String, CompletionStage<Done>> sink = Sink.foreach(System.out::println);
 
+//		alternativy pro sink
+//		Sink.ignore(); //nic nedela, ignoruje co do nej prijde
+		
 		//vytvori graf
 		RunnableGraph<NotUsed> graph = source.via(flow).to(sink);
 		
 		ActorSystem actor = ActorSystem.create(Behaviors.empty(), "actorSystem");
-		graph.run(actor);
+		graph.run(actor); //actor system po spusteni bude stale spusteny :/
 		
-		//actor system po spusteni bude stale spusteny :/
+//		alternativni zjednuseny zapis
+//		sink.runWith(source, actor); //zkraceny zapis bez grafu
+//		source.via(flow).runForeach(System.out::println, actor); //zkraceny zapis bez sinku
 	}
 
 }
