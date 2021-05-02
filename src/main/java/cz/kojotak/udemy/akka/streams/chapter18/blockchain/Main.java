27c740a11b4b4884268ccf742137977329645ae7
kojotak <tomas.beneda@gmail.com>
2021-05-02 09:35:28 +0200

    akka streams chapter 18

---
+++ src/main/java/cz/kojotak/udemy/akka/streams/chapter18/blockchain/Main.java
@@ -0,0 +1,12 @@
+package cz.kojotak.udemy.akka.streams.chapter18.blockchain;
+
+public class Main {
+		
+	public static void main(String[] args) {
+			
+		BlockChainMiner miner = new BlockChainMiner();
+		miner.mineBlocks();
+		
+	}
+	
+}
---
+++ src/main/java/cz/kojotak/udemy/akka/streams/chapter18/model/BlockValidationException.java
@@ -0,0 +1,5 @@
+package cz.kojotak.udemy.akka.streams.chapter18.model;
+
+public class BlockValidationException extends Exception {
+
+}
