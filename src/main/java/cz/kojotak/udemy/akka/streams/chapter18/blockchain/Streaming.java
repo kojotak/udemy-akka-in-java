b6dbd644b54708a71c7987fb2460a9dac4adc7b6
kojotak <tomas.beneda@gmail.com>
2021-05-02 10:08:35 +0200

    akka streams chapter 18 walkthtough for exercise 4

--- src/main/java/cz/kojotak/udemy/akka/streams/chapter18/blockchain/Streaming.java
+++ src/main/java/cz/kojotak/udemy/akka/streams/chapter18/blockchain/Streaming.java
@@ -1,6 +1,8 @@
 package cz.kojotak.udemy.akka.streams.chapter18.blockchain;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import akka.NotUsed;
@@ -25,7 +27,17 @@ public class Streaming {
 					System.out.println("received transaction " + transId);
 					return new Transaction(transId, System.currentTimeMillis(), random.nextInt(1000), random.nextDouble() * 100);
 				});
-		Flow<Transaction,Block,NotUsed> blockBuilder = null;
+		Flow<Transaction,Block,NotUsed> blockBuilder = Flow.of(Transaction.class).map(trans->{
+			List<Transaction> list = new ArrayList<>();
+			list.add(trans);
+			Block block = new Block("0", list);
+			System.out.println("created block: " + block);
+			return block;
+		}).conflate( (block1, block2)->{
+			block1.addTransactionToList(block2.getFirstTransaction());
+			System.out.println("conflated block: " + block1);
+			return block1;
+		});
 		Flow<Block, Block, NotUsed> miningProcess = Flow.of(Block.class).map(block->{
 			System.out.println("starting to mine "  + block.toString() );
 			Thread.sleep(10000);
